e6b40d9f7f1efc3542625707d0638e5538e4290e
Serg Basin <shoom.pozitiv@gmail.com>
2021-03-26 12:58:45 +0200

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Sergey Basin on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
